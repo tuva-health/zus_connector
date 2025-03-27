2202efc677fc6c5773c661a3065108bb515092c0
thutva <thuxuan@tuvahealth.com>
2025-03-27 13:01:24 -0400

    initial build of a connector template

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# connector_template
+This is a template to create connectors for The Tuva Project on version `0.14.x`
+
+### Tuva Resources:
+- The Tuva Project [docs](https://thetuvaproject.com/)
+- The Tuva Project [dbt docs](https://tuva-health.github.io/tuva/#!/overview/)
+- The Tuva Project [package](https://hub.getdbt.com/tuva-health/the_tuva_project/latest/)
+
+### dbt Resources:
+- Learn more about dbt [in the docs](https://docs.getdbt.com/docs/introduction)
+- Check out [Discourse](https://discourse.getdbt.com/) for commonly asked questions and answers
+- Join the [chat](https://community.getdbt.com/) on Slack for live discussions and support
+- Find [dbt events](https://events.getdbt.com) near you
+- Check out [the blog](https://blog.getdbt.com/) for the latest news on dbt's development and best practices
---
+++ models/final/pharmacy_claim.sql
@@ -0,0 +1,27 @@
+select
+      cast(null as {{ dbt.type_string() }}) as claim_id
+    , cast(null as {{ dbt.type_string() }}) as claim_line_number
+    , cast(null as {{ dbt.type_string() }}) as person_id
+    , cast(null as {{ dbt.type_string() }}) as member_id
+    , cast(null as {{ dbt.type_string() }}) as payer
+    , cast(null as {{ dbt.type_string() }}) as plan
+    , cast(null as {{ dbt.type_string() }}) as prescribing_provider_npi
+    , cast(null as {{ dbt.type_string() }}) as dispensing_provider_npi
+    , cast(null as {{ dbt.type_string() }}) as dispensing_date
+    , cast(null as {{ dbt.type_string() }}) as ndc_code
+    , cast(null as {{ dbt.type_string() }}) as quantity
+    , cast(null as {{ dbt.type_string() }}) as days_supply
+    , cast(null as {{ dbt.type_string() }}) as refills
+    , cast(null as {{ dbt.type_string() }}) as paid_date
+    , cast(null as {{ dbt.type_string() }}) as paid_amount
+    , cast(null as {{ dbt.type_string() }}) as allowed_amount
+    , cast(null as {{ dbt.type_string() }}) as charge_amount
+    , cast(null as {{ dbt.type_string() }}) as coinsurance_amount
+    , cast(null as {{ dbt.type_string() }}) as copayment_amount
+    , cast(null as {{ dbt.type_string() }}) as deductible_amount
+    , cast(null as {{ dbt.type_string() }}) as in_network_flag
+    , cast(null as {{ dbt.type_string() }}) as data_source
+    , cast(null as {{ dbt.type_string() }}) as file_name
+    , cast(null as {{ dbt.type_string() }}) as file_date
+    , cast(null as {{ dbt.type_string() }}) as ingest_datetime
+
